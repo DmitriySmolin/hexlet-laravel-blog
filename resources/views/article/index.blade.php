fbaa09f8e79ad8f5972e6a7a293fad0bfc29f8b4
DmitriySmolin <>
2023-12-19 10:02:06 +0000

    add Update CRUD

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,6 +5,7 @@
     @foreach ($articles as $article)
         <h2><a href="{{route('articles.show', $article)}}">{{$article->name}}</a></h2>
         <div>{{$article->body}}</div>
+        <small><a href="{{route('articles.edit', $article)}}">Редактировать статью</a></samll>
     @endforeach
 @endsection
 
